58c9ba7e0fa0fea12832e0e705131e4d994ff00c
Ellory <edoyle@smith.edu>
2023-03-29 11:26:08 -0400

    Almost Finished with Library

--- House.java
+++ House.java
@@ -1,12 +1,82 @@
+import java.util.ArrayList;
 /* This is a stub for the House class */
-public class House {
+public class House extends Building {
 
-  public House() {
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
 
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
+  }
+
+  public int nResidents(){
+    return this.residents.size();
+  }
+
+  public void moveIn(String name){
+    if (this.residents.contains(name)){
+      throw new RuntimeException(name + " is already a resident of " + this.name + ".");
+    }
+    this.residents.add(name);
+    System.out.println(name + " has just moved into " + this.name + "! Go say hello :-)");
+  }
+
+  public String moveOut(String name){
+    if (!this.residents.contains(name)){
+      throw new RuntimeException(name + " is not a resident of " + this.name + ".");
+    }
+    this.residents.remove(name);
+    System.out.println(name + " has just moved out of " + this.name + ". I hope you said your goodbyes!");
+    return name;
+  }
+
+  public boolean isResident(String person){
+    if (!this.residents.contains(person)){
+      System.out.println(person  + " is not a resident of " + this.name + ".");
+      return false;
+    }
+    if (this.residents.contains(person)){
+      System.out.println(person + " is a resident of " + this.name + ".");
+      return true;
+    }
+    else{
+      throw new RuntimeException("Something went wrong. Please check inputs.");
+    }
+  }
+  public String toString() {
+    String description = super.toString();
+    if (this.nResidents() == 1){
+      description += " There is currently " + this.nResidents() + " person living in this house.";
+    }
+    else {
+      description += " There are currently " + this.nResidents() + " people living in this house.";
+    }
+    description += " This house ";
+    if (this.hasDiningRoom) {
+      description += "has";
+    } else {
+      description += "does not have";
+    }
+    description += " an active dining room.";
+    return description;
+  }
+
   public static void main(String[] args) {
-    new House();
+    House morrow = new House("Morrow", "The Quad", 4, false);
+    System.out.println(morrow);
+    morrow.moveIn("Jordan");
+    System.out.println(morrow);
+    House king = new House("King", "The Quad", 3, true);
+    System.out.println(king);
+    morrow.isResident("Jordan");
+    king.isResident("Jordan");
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,96 @@
+import java.util.Hashtable;
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building {
 
-    public Library() {
+  private Hashtable <String, Boolean> collection;
+  public boolean containsTitle;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      Hashtable <String, Boolean> collection = new Hashtable<>();
       System.out.println("You have built a library: 📖");
     }
-  
-    public static void main(String[] args) {
-      new Library();
+
+    public void addTitle (String title){
+      if (this.collection.contains(title)){
+        throw new RuntimeException(title + " is already a part of " + this.name + "'s catalogue.");
+      }
+      this.collection.put(title,true);
+      System.out.println(title + " has been added to " + this.name + "'s catalogue. Give it a read!");
     }
   
+    public String removeTitle (String title){
+      if (!this.collection.contains(title)){
+        throw new RuntimeException ("This title is not a part of " + this.name + "'s cataloge.");
+      }
+      this.collection.remove(title);
+      System.out.println(title + " has been removed from " + this.name + "'s catalogue. Hope you got to read it!");
+      return title;
+    }
+
+    public void checkOut (String title){
+      if (!this.collection.contains(title)){
+        throw new RuntimeException (title + " is not a part of " + this.name + "'s cataloge.");
+      }
+      this.collection.replace(title, false);
+    }
+
+    public void returnBook (String title){
+      if (!this.collection.contains(title)){
+        throw new RuntimeException (title + " is not a part of " + this.name + "'s cataloge.");
+      }
+      this.collection.replace(title, true);
+    }
+
+    public boolean containsTitle (String title){
+      if (this.collection.contains(title)){
+        System.out.println(title + " is a part of " + this.name + "'s cataloge.");
+        return true;
+      }
+      else{
+        System.out.println(title + " is not a part of " + this.name + "'s cataloge.");
+        return false;
+      }
+    }
+
+    public boolean isAvailable (String title){
+      if (!this.collection.contains(title)){
+        throw new RuntimeException (title + " is not a part of " + this.name + "'s cataloge.");
+      }
+      if (this.collection.get(title) == false){
+        System.out.println(title + " is not currently available.");
+        return false;
+      }
+      else{
+        System.out.println(title + " is currently available.");
+        return true;
+      }
+    }
+
+    public void printCollection(){
+      /////
+      this.collection.entrySet().forEach( entry -> {
+        System.out.println(entry.getKey() + ": ");
+        if (entry.getValue() == true){
+          System.out.println("available");
+        }
+        if(entry.getValue() == false){
+          System.out.println("unavailable");
+        }
+      });
+    }
+
+    public String toString() {
+      String description = super.toString();
+      return description;
+    }
+    public static void main(String[] args) {
+      Library williamMcKinley = new Library("William McKinley Public Library", "Lima, OH", 3);
+      System.out.println(williamMcKinley);
+      williamMcKinley.addTitle("Super Bass");
+      williamMcKinley.containsTitle("Super Bass");
+      williamMcKinley.containsTitle("Not So Super Bass");
+      williamMcKinley.removeTitle("Super Bass");
+      williamMcKinley.removeTitle("Not So Super Bass");
+    }
   }
